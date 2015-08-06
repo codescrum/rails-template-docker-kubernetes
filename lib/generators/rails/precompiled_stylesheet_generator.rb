a82b4ef064da315634162f6312548ae69a81843e
Johan Tique <johan.tique@gmail.com>
2015-08-06 17:56:45 -0500

    Update precompiled_stylesheet_generator.rb

--- lib/generators/rails/precompiled_stylesheet_generator.rb
+++ lib/generators/rails/precompiled_stylesheet_generator.rb
@@ -7,7 +7,7 @@ module Rails
       def add_stylesheet_to_precompilation_assets
         inside 'config' do
           inside 'initializers' do
-            insert_into_file 'assets.rb', " #{File.join(Sassish.relative_stylesheet_directory_path, "#{name}.css")} ", after: 'Rails.application.config.assets.precompile += %w('
+            insert_into_file 'assets.rb', " #{File.join(Sassish.relative_stylesheet_directory_path, "#{name.underscore}.css")} ", after: 'Rails.application.config.assets.precompile += %w('
           end
         end
       end
